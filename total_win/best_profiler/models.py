f0c92902c5510390ca0efeda8035877da725aa26
HuxleyP <zolotukhin.lay@gmail.com>
2020-04-28 22:55:39 +0300

    End of day 0 (ноль (Zero))

--- total_win/best_profiler/models.py
+++ total_win/best_profiler/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Grades(models.Model):
+    id = models.IntegerField(primary_key=True)
+    grade = models.IntegerField()
+    date = models.DateTimeField()
+    subject = models.CharField(max_length=12)
+    user_id = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+class Olympics(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=12)
+    description = models.TextField() # Описание олимпиады/хакатона
+    date = models.DateTimeField() # Дата проведения
+    theme = models.CharField(max_length=12) # Предмет, по которому проводится олимпиада
+
+class OlympicsUsers(models.Model):
+    olympic_id = models.ForeignKey(to=Olympics, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
+    score = models.IntegerField()
+
+
+
+class OtherAchievements(models.Model):
+    description = models.TextField()
+    theme = models.CharField(max_length=12)
--- total_win/best_profiler/views.py
+++ total_win/best_profiler/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello world!")
+
+def avg_grade(id, subject):
+    Grades = Grades.object.filter(user_id=request.user)
+    sum = 0
+    for i in range(len(Grades)):
+        sum += Grades[i].grade
+    return sum / len(Grades)
+
+def direction():
+    
