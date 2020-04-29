f2dc921c90dd62d47c7af31b19400d54f14b1768
HuxleyP <zolotukhin.lay@gmail.com>
2020-04-29 15:56:16 +0300

    шайсе

--- total_win/best_profiler/views.py
+++ total_win/best_profiler/views.py
@@ -5,12 +5,21 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello world!")
 
-def avg_grade(id, subject):
+def AvgGrade(id, subject):
     Grades = Grades.object.filter(user_id=request.user)
     sum = 0
     for i in range(len(Grades)):
         sum += Grades[i].grade
     return sum / len(Grades)
 
-def direction():
-    
+def direction(id):
+    MathAvg = AvgGrade(id, "math")
+    PhysicsAvg = AvgGrade(id, "physics")
+    InformaticsAvg = AvgGrade(id, "informatics")
+    RussianAvg = AvgGrade(id, "rus")
+    EnglishAvg = AvgGrage(id, "en")
+    FrenchAvg = AvgGrade(id, "fr")
+    #TODO И так далее
+    MaxMark = max(MathAvg+PhysicsAvg, MathAvg+InformaticsAvg) 
+    if (MathAvg+PhysicsAvg) >
+         score = MathAvg+PhysicsAvg / 2
