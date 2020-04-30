2298b558ee4e430556ed061df5bf34f65a8f5ed2
HuxleyP <zolotukhin.lay@gmail.com>
2020-04-30 15:18:05 +0300

    efefe

---
+++ total_win/BestProfiler/menu.py
@@ -0,0 +1,85 @@
+class MenuItem:
+    def __init__(self, name, title, url):
+        self.name = name
+        self.title = title
+        self.url = url
+
+        self.is_active = False
+        self.is_dropdown = False
+        self.is_right = True
+
+
+class DropdownItem(MenuItem):
+    def __init__(self, name, title, url):
+        super().__init__(name, title, url)
+
+        self.items = []
+        self.is_dropdown = True
+
+
+REGISTER_PAGE_NAME = "register"
+LOGIN_PAGE_NAME = "login"
+USER_PAGE_NAME = "user"
+LOGOUT_PAGE_NAME = "logout"
+
+HOME_PAGE_NAME = "index"
+
+USER_VIEW_PAGE_NAME = "user"
+
+login_menu_item = MenuItem(LOGIN_PAGE_NAME, "Вход", "/login")
+register_menu_item = MenuItem(REGISTER_PAGE_NAME, "Регистрация", "/register")
+
+my_user_menu_item = MenuItem(LOGOUT_PAGE_NAME, "Профиль", "/user/")
+logout_menu_item = MenuItem(LOGOUT_PAGE_NAME, "Выход", "/logout")
+
+home_menu_item = MenuItem(HOME_PAGE_NAME, "Главная", "/")
+main_menu_item = MenuItem(USER_VIEW_PAGE_NAME, "Рейтинг пользователей", "/user")
+
+# user_votes_menu_item = MenuItem(USER_VOTES_PAGE_NAME, "История Голосований", "/votes/my")
+# votes_editor_menu_item = MenuItem(VOTES_EDITOR_PAGE_NAME, "Редактор Голосований", "/votes/editor")
+
+
+def get_context_menu(request, current_name):
+    return {"left": get_context_left_menu(request, current_name),
+            "right": get_context_right_menu(request, current_name)}
+
+
+def get_context_left_menu(request, current_name):
+    menu = [home_menu_item]
+
+    if request.user.is_authenticated:
+        menu.append(main_menu_item)
+        menu.append(home_menu_item)
+    else:
+        menu.append(login_menu_item)
+        menu.append(register_menu_item)
+
+    for item in menu:
+        if item.name == current_name:
+            item.is_active = True
+        else:
+            item.is_active = False
+
+    return menu
+
+
+def get_context_right_menu(request, current_name):
+    menu = []
+
+    if request.user.is_authenticated:
+        my_user_menu_item.url = '/user/' + str(request.user.id)
+
+        account_menu_item = DropdownItem("account", "Аккаунт", "")
+        account_menu_item.items.append(my_user_menu_item)
+        account_menu_item.items.append(logout_menu_item)
+
+        account_menu_item.title = request.user.username
+        menu.append(account_menu_item)
+
+    for item in menu:
+        if item.name == current_name:
+            item.is_active = True
+        else:
+            item.is_active = False
+
+    return menu
--- total_win/BestProfiler/views.py
+++ total_win/BestProfiler/views.py
@@ -5,98 +5,9 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from BestProfiler.forms import RegisterForm, LoginForm
 from .models import Grades
-
+from BestProfiler.menu import get_context_menu, REGISTER_PAGE_NAME, LOGIN_PAGE_NAME, USER_PAGE_NAME, USER_VIEW_PAGE_NAME, HOME_PAGE_NAME
 
 # Create your views here.
-
-def index(request):
-    if request.user.is_authenticated:
-        return redirect('/main_page')
-    else:
-        return redirect('/login')
-
-
-def register_view(request):  # Регистрация в системе
-    context = {}
-    if request.method == 'POST':
-        errors = []
-        register_form = RegisterForm(request.POST or None)
-        if register_form.is_valid():
-            login_data = register_form.cleaned_data['login']
-            password_data = register_form.cleaned_data['password']
-            password_repeat_data = register_form.cleaned_data['password_repeat']
-
-            context['form'] = RegisterForm()
-
-            print(login_data, password_data)
-
-            # if len(login_data) < 5:
-            #    errors.append('Логин должен иметь длину больше 5 символов')
-
-            if User.objects.filter(username=login_data).exists():
-                errors.append('Пользователь с таким логином уже существует')
-
-            # if len(password_data) < 6:
-            #    errors.append('Пароль должен иметь длину больше 6 символов')
-
-            if password_data != password_repeat_data:
-                errors.append('Пароли не совпадают')
-
-            if len(errors) == 0:
-                user = User.objects.create_user(username=login_data, password=password_data)
-                user.save()
-
-                if user is not None:
-                    # login(request, user)
-                    # return redirect('/login')
-                    return redirect('/main_page')
-
-                return redirect('/main_page')
-        else:
-            errors.append('Заполните все поля')
-        context['error'] = errors[0]
-    else:
-        context['form'] = RegisterForm()
-    return render(request, 'register_auth.html', context)
-
-
-def login_view(request):  # Вход в систему
-    context = {}
-    if request.method == 'POST':
-        login_form = LoginForm(request.POST)
-
-        if login_form.is_valid():
-            login_data = login_form.cleaned_data['login']
-            password_data = login_form.cleaned_data['password']
-
-            print(login_data)
-            print(password_data)
-
-            user = authenticate(request, username=login_data, password=password_data)
-
-            if user is not None:
-                login(request, user)
-                return redirect('/main_page')
-
-        context['error'] = 'Неверный логин или пароль'
-
-        return render(request, 'login.html', context)
-    else:
-        return render(request, 'login.html', {"form": LoginForm()})
-
-
-def mainpage_view(request):
-    context = {"users": User.objects.all()}
-    return render(request, 'main.html', context)
-
-
-@login_required
-def logout_view(request):
-    logout(request)
-    print("here")
-    return redirect('/')
-
-
 def AvgGrade(id, subject):
     grade = Grades.objects.filter(user_id=id).filter(subject=subject)
     sum = 0
@@ -140,22 +51,118 @@ def direction(id):
     return score, orientation
 
 
+def index(request):
+    if request.user.is_authenticated:
+        return redirect('/main_page')
+    else:
+        return redirect('/login')
+
+
+def register_view(request):  # Регистрация в системе
+    context = {'menu': get_context_menu(request, REGISTER_PAGE_NAME)}
+    if request.method == 'POST':
+        errors = []
+        register_form = RegisterForm(request.POST or None)
+        if register_form.is_valid():
+            login_data = register_form.cleaned_data['login']
+            password_data = register_form.cleaned_data['password']
+            password_repeat_data = register_form.cleaned_data['password_repeat']
+
+            context['form'] = RegisterForm()
+
+            print(login_data, password_data)
+
+            # if len(login_data) < 5:
+            #    errors.append('Логин должен иметь длину больше 5 символов')
+
+            if User.objects.filter(username=login_data).exists():
+                errors.append('Пользователь с таким логином уже существует')
+
+            # if len(password_data) < 6:
+            #    errors.append('Пароль должен иметь длину больше 6 символов')
+
+            if password_data != password_repeat_data:
+                errors.append('Пароли не совпадают')
+
+            if len(errors) == 0:
+                user = User.objects.create_user(username=login_data, password=password_data)
+                user.save()
+
+                if user is not None:
+                    # login(request, user)
+                    # return redirect('/login')
+                    return redirect('/main_page')
+
+                return redirect('/main_page')
+        else:
+            errors.append('Заполните все поля')
+        context['error'] = errors[0]
+    else:
+        context['form'] = RegisterForm()
+    return render(request, 'register.html', context)
+
+
+def login_view(request):  # Вход в систему
+    context = {'menu': get_context_menu(request, LOGIN_PAGE_NAME)}
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+
+        if login_form.is_valid():
+            login_data = login_form.cleaned_data['login']
+            password_data = login_form.cleaned_data['password']
+
+            print(login_data)
+            print(password_data)
+
+            user = authenticate(request, username=login_data, password=password_data)
+
+            if user is not None:
+                login(request, user)
+                return redirect('/main_page')
+
+        context['error'] = 'Неверный логин или пароль'
+
+        return render(request, 'login.html', context)
+    else:
+        context['form'] = LoginForm()
+        return render(request, 'login.html', context)
+
+
+def mainpage_view(request):
+    context = {'menu': get_context_menu(request, HOME_PAGE_NAME)}
+
+    score, orientation = direction(user_id)
+    context['score'] = score
+    context['orientation'] = orientation
+
+    context = {"users": User.objects.all()}
+    return render(request, 'main.html', context)
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    print("here")
+    return redirect('/')
+
+
 @login_required
 def user_page_view(request, user_id):
-    context = {}
+    context = {'menu': get_context_menu(request, USER_PAGE_NAME)}
 
     user = User.objects.get(pk=user_id)
 
     perf = Grades.objects.filter(user_id=user_id)
-    s=set()
+    s = set()
     for i in perf:
         s.add(i.subject)
-    overall = direction(user_id)
+    score, orientation = direction(user_id)
 
     context['user'] = user
-    context['predmet']=s
+    context['predmet'] = s
     context["perfomance"] = perf
 
-    context['overall'] = overall
+    context['score'] = score
+    context['orientation'] = orientation
 
     return render(request, 'user.html', context)
